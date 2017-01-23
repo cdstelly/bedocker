e3ab77b38fa7db74f552ac69d88ddffd87354c44
Christopher Stelly <cstelly1@uno.edu>
2017-01-22 21:31:43 -0600

    Added stats

--- src/rpcclient/client.go
+++ src/rpcclient/client.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"io/ioutil"
 	"fmt"
+	"io/ioutil"
 	"net/rpc"
-//	"net/http"
+	//	"net/http"
 	"log"
 	"rpcshared"
+
+	"github.com/montanaflynn/stats"
 )
 
 func main() {
@@ -16,8 +18,8 @@ func main() {
 	}
 
 	filepath := "test.file"
-	fileData, err:= ioutil.ReadFile(filepath)
-	if err != nil { 
+	fileData, err := ioutil.ReadFile(filepath)
+	if err != nil {
 		log.Fatal("error reading file: ", err)
 	}
 
@@ -28,6 +30,20 @@ func main() {
 		log.Fatal("be error:", err)
 	}
 	fmt.Printf("Result: %s\n", reply)
+
+	// Get the stats of the RPC client
+	getArgs := &rpcshared.Args{}
+	var ExecutionHistory []float64
+	getErr := client.Call("BulkExtractor.GetHistory", getArgs, &ExecutionHistory)
+	if getErr != nil {
+		fmt.Println("Error getting history: ", getErr)
+	}
+	theMean, mathErr := stats.Mean(ExecutionHistory)
+	theSum, mathErr := stats.Sum(ExecutionHistory)
+	theMax, mathErr := stats.Max(ExecutionHistory)
+	if mathErr != nil {
+		fmt.Println("Arithmitic error: ", mathErr)
+	}
+
+	fmt.Printf("The average time it takes to run BulkExtract: %f\tSum: %f\tMax: %f\n", theMean, theSum, theMax)
 }
-
-
--- src/rpcshared/shared.go
+++ src/rpcshared/shared.go
@@ -1,17 +1,21 @@
 package rpcshared
 
 import (
-	"strings"
-    "fmt"
-    "os/exec"
-	"os"
-    "log"
-    "bytes"
-	"io/ioutil"
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
-type BulkExtractor string
+type BulkExtractor struct {
+	NumberRequests int
+	RequestHistory []float64
+}
 
 type Args struct {
 	Data   []byte
@@ -19,10 +23,10 @@ type Args struct {
 }
 
 func (t *BulkExtractor) Extract(args *Args, reply *string) error {
-    pathToTool := "/usr/local/bin/bulk_extractor"
-    fmt.Println("Path to BulkExtractor: " , pathToTool)
+	pathToTool := "/usr/local/bin/bulk_extractor"
+	fmt.Println("Path to BulkExtractor: ", pathToTool)
 
-	bulk_Input_Directory  := "/tmp/bulk_in/"  + args.DataID + "/"
+	bulk_Input_Directory := "/tmp/bulk_in/" + args.DataID + "/"
 	bulk_Output_Directory := "/tmp/bulk_out/" + args.DataID + "/"
 
 	// Bulk Extractor works by inspecting data in a directory (or a raw disk dump), setup the passed in data as such:
@@ -39,22 +43,24 @@ func (t *BulkExtractor) Extract(args *Args, reply *string) error {
 	//Should look like the following: /usr/local/bin/bulk_extractor -m 1 -R /tmp/bulk_in/ -o /tmp/bulk_out/
 	cmd := exec.Command(pathToTool, opts...)
 
-
 	//Capture STDOUT
-    var out bytes.Buffer
-    cmd.Stdout = &out
+	var out bytes.Buffer
+	cmd.Stdout = &out
 
-	// Actually run the command: 
-    err = cmd.Run()
+	// Setup a timer
+	startTime := time.Now()
+
+	// Actually run the command:
+	err = cmd.Run()
 	fmt.Println("[-] Output: ", out.String())
 
 	//Post process the BE output
 	jsonMapping := make(map[string]string)
 	files, err := ioutil.ReadDir(bulk_Output_Directory)
-    for _, f := range files {
+	for _, f := range files {
 		if f.Size() > 0 {
 			fmt.Println("File ", f.Name(), " is not null")
-			filedata,ferr := ioutil.ReadFile(bulk_Output_Directory + f.Name())
+			filedata, ferr := ioutil.ReadFile(bulk_Output_Directory + f.Name())
 			if ferr != nil {
 				fmt.Println("was not able to open file at: ", f.Name(), ferr)
 			}
@@ -63,27 +69,36 @@ func (t *BulkExtractor) Extract(args *Args, reply *string) error {
 			jsonValue := "bulkextract-" + string(filedata)
 			jsonMapping[jsonKey] = jsonValue
 		}
-    }
+	}
 
 	// Dump everything into JSON in preperation for Elasticsearch upload
 	jsonString, err := json.Marshal(jsonMapping)
-    if err != nil {
-            log.Println(err)
+	if err != nil {
+		log.Println(err)
 	}
 	fmt.Println(string(jsonString))
 
 	//We want to return the JSON in addition to STDOUT
 	*reply = out.String()
-    if err != nil {
-            log.Println(err)
-    }
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Save execution time to history
+	executionTime := time.Since(startTime).Seconds() //use seconds as opposed to nanoseconds, returns float64 which is required with stats package
+	t.NumberRequests += 1
+	t.RequestHistory = append(t.RequestHistory, executionTime)
 
 	// If all goes well, remove temp directories
 	remerr := os.RemoveAll(bulk_Input_Directory)
-	remerr  = os.RemoveAll(bulk_Output_Directory)
+	remerr = os.RemoveAll(bulk_Output_Directory)
 	if remerr != nil {
 		fmt.Println("Error cleaning up temporary directories: ", remerr)
 	}
-    return nil
+	return nil
 }
 
+func (t *BulkExtractor) GetHistory(args *Args, reply *[]float64) error {
+	*reply = t.RequestHistory
+	return nil
+}
